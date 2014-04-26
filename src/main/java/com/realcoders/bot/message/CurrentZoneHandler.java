aa1989b86470ce14777c01541f368885ed541a75
Tomek Kozlowski <tkozlowski007@gmail.com>
2014-04-26 17:39:29 +0200

    removing zone

--- src/main/java/com/realcoders/bot/message/CurrentZoneHandler.java
+++ src/main/java/com/realcoders/bot/message/CurrentZoneHandler.java
@@ -1,6 +1,10 @@
 package com.realcoders.bot.message;
 
 import com.realcoders.bot.Game;
+import com.realcoders.bot.Region;
+import com.realcoders.bot.network.MessageMapper;
+
+import java.util.Map;
 
 /**
  * Created by tomek on 4/26/14.
@@ -8,6 +12,13 @@ import com.realcoders.bot.Game;
 public class CurrentZoneHandler extends MessageHandler {
     @Override
     public void handle(String message, Game game) {
-        System.out.println("MSG CURR ZONE: " + message);
+        String zone = message.substring(message.indexOf(' ') + 1);
+        System.out.println("Zone to remove: " + zone);
+        for (Map.Entry<Region, String> entries : MessageMapper.regionMap.entrySet()) {
+            if (entries.getValue().equals(zone)) {
+                game.removeRegion(entries.getKey());
+            }
+        }
+
     }
 }
