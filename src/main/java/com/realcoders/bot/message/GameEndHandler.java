f02fe0cfa62a18e0980904fa9b4da292d5665f01
Bruno Panasiewicz <ciuak.prog@gmail.com>
2014-04-26 23:30:38 +0200

    quit on quit

--- src/main/java/com/realcoders/bot/message/GameEndHandler.java
+++ src/main/java/com/realcoders/bot/message/GameEndHandler.java
@@ -7,12 +7,14 @@ import com.realcoders.bot.network.MessageMapper;
  * Created by tomek on 4/26/14.
  */
 public class GameEndHandler extends MessageHandler {
-    @Override
-    public void handle(String message, Game game) {
-        if (message.contains(MessageMapper.BOT_ALIAS)) {
-            // notify that we have won
-        } else {
-            // tie
-        }
-    }
+	@Override
+	public void handle(String message, Game game) {
+		if (message.contains(MessageMapper.BOT_ALIAS)) {
+			System.out.println("WYGRALISMY!");
+			Runtime.getRuntime().exit(0);
+		} else {
+			System.out.println("PREZGRALISMY?");
+			Runtime.getRuntime().exit(0);
+		}
+	}
 }
